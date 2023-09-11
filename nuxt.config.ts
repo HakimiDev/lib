f9e41ff6bba5f89327cfaff8087fdcc457c79cf2
HakimiDev <hakimideveloper@gmail.com>
2023-09-11 21:01:24 +0300

    fix bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,11 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: "Hakimi",
-      meta: [{ name: "description", content: "Everything about Hakimi" }],
+      meta: [
+        { name: "description", content: "Everything about Hakimi" },
+        { name: "charset", content: "UTF-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" }
+    ],
       link: [
         {
           rel: "stylesheet",
